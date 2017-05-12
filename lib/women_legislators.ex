1f04113d8e040093ebe13208f7c07ac812555bde
Brett Wise <brettdwise@gmail.com>
2017-05-12 00:34:16 -0400

    Initial commit

---
+++ lib/women_legislators.ex
@@ -0,0 +1,18 @@
+defmodule WomenLegislators do
+  @moduledoc """
+  Documentation for WomenLegislators.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WomenLegislators.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/women_legislators_test.exs
@@ -0,0 +1,8 @@
+defmodule WomenLegislatorsTest do
+  use ExUnit.Case
+  doctest WomenLegislators
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
